1379002d003d1a0b5203d6484f86eb9c38e0e903
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-08-24 15:54:36 -0300

    chore: revert actor header `iota_wallet_ffi.h`

--- bindings/swift/iota_wallet_ffi.h
+++ bindings/swift/iota_wallet_ffi.h
@@ -1,22 +1,18 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-#include <stdint.h>
-#include <stddef.h>
+#import <Foundation/Foundation.h>
+
+typedef void (*Callback)(const char *response);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
-typedef struct iota_wallet_handle iota_wallet_handle_t;
-
-typedef void (*Callback)(const char* response, const char* error, void* context);
-
-extern iota_wallet_handle_t* iota_initialize(const char* manager_options, char* error_buffer, size_t error_buffer_size);
-extern void iota_destroy(iota_wallet_handle_t*);
-extern void iota_send_message(iota_wallet_handle_t* wallet_handle, const char* message, Callback callback, void* context);
-extern int8_t iota_listen(iota_wallet_handle_t* wallet_handle, const char* event_types, Callback callback, void* context, char* error_buffer, size_t error_buffer_size);
+void iota_initialize(Callback callback, const char *actor_id, const char *storage_path);
+void iota_destroy(const char *actor_id);
+void iota_send_message(const char *message);
+void iota_listen(const char *actor_id, const char *id, const char *event_name);
 
 #ifdef __cplusplus
 }
